a654511c6fdde43380074aa215db148280df8657
Chris Pollard <cspollard@gmail.com>
2016-10-30 17:10:31 +0000

    more simplification

---
+++ src/Data/Atlas/Histogramming.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Atlas.Histogramming where
+
+import Control.Lens
+import qualified Control.Foldl as F
+
+import Data.Semigroup
+
+import Data.Maybe (listToMaybe)
+import Data.Foldable (toList)
+
+import Data.Text (Text)
+
+import Data.YODA.Obj
+
+toFold :: (b -> a -> b) -> b -> F.Fold a b
+toFold f o = F.Fold f o id
+
+feed :: F.Fold a b -> a -> F.Fold a b
+feed (F.Fold f o g) x = let o' = f o x in o' `seq` F.Fold f o' g
+
+lseq :: [a] -> [a]
+lseq [] = []
+lseq (x:xs) = seq x $ x : lseq xs
+
+
+foldAll :: F.Foldable f => F.Fold a b -> F.Fold (f a) b
+foldAll = F.handles folded
+
+foldFirst :: F.Foldable f => F.Fold a b -> F.Fold (f a) b
+foldFirst f = F.premap (listToMaybe . toList) (foldAll f)
+
+foldIf :: (a -> Bool) -> F.Fold a b -> F.Fold a b
+foldIf g f = F.premap g' $ foldAll f
+    where g' x = if g x then Just x else Nothing
+
+fillOver :: Fillable a => Traversal' b a -> b -> F.Fold (Weight a, FillVec a) b
+fillOver l = toFold (\y (w, x) -> over l (filling w x) y)
+
+-- not sure about these fixities.
+infixl 2 <$=
+(<$=) :: F.Fold c b -> (a -> c) -> F.Fold a b
+(<$=) = flip F.premap
+
+
+infixr 3 =:=
+(=:=) :: Applicative f => f a -> f [a] -> f [a]
+fx =:= fxs = (:) <$> fx <*> fxs
+
+
+infixr 3 =++=
+(=++=) :: Applicative f => f [a] -> f [a] -> f [a]
+fxs =++= fys = (++) <$> fxs <*> fys
+
+
+dsigdXpbY :: Text -> Text -> Text
+dsigdXpbY x y = "$\\frac{d\\sigma}{d" <> x <> "} \\frac{\\mathrm{pb}}{" <> y <> "}$"
+
+gev, rad, pt :: Text
+gev = "\\mathrm{GeV}"
+rad = "\\mathrm{rad}"
+pt = "p_{\\mathrm{T}}"
+
+
+yodaHist :: Int -> Double -> Double -> Text -> Text -> Text -> YodaObj
+yodaHist nb xmin xmax p xl yl = yodaHist1D nb xmin xmax
+    & annots . at "Path"   ?~ p
+    & annots . at "XLabel" ?~ xl
+    & annots . at "YLabel" ?~ yl
+
+yodaProf :: Int -> Double -> Double -> Text -> Text -> Text -> YodaObj
+yodaProf nb xmin xmax p xl yl = yodaProf1D nb xmin xmax
+    & annots . at "Path"   ?~ p
+    & annots . at "XLabel" ?~ xl
+    & annots . at "YLabel" ?~ yl
+
