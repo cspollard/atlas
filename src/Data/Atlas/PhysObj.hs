4133f08148f8acc86ec997452c3c7239b9667fc8
Chris Pollard <cspollard@gmail.com>
2017-03-16 18:06:05 +0000

    merge

--- src/Data/Atlas/PhysObj.hs
+++ src/Data/Atlas/PhysObj.hs
@@ -2,30 +2,23 @@
 
 module Data.Atlas.PhysObj where
 
-import           Control.Monad              (join)
+import           Control.Monad             (join)
+import           Control.Monad.Trans.Cont
 import           Control.Monad.Trans.Maybe
-import           Control.Monad.Trans.Writer
 import           Data.Atlas.Corrected
 import           Data.Atlas.Variation
-import           Data.Bifunctor             (second)
+import           Data.Bifunctor            (second)
 
 -- an MC object:
 -- we have variations on the weights of the object
 -- as well as variations on the object iself
 -- when we run the whole thing, these Vars are combined.
-type PhysObj = WriterT (Vars SF) (MaybeT Vars)
 
-setWgt :: Monad m => Vars SF -> WriterT (Vars SF) m ()
-setWgt = tell
+type PhysObj r = ContT r (MaybeT (CorrectedT (Vars SF) Vars))
 
-runPhysObj :: PhysObj a -> Vars (Maybe (a, Double))
-runPhysObj =
-  join
-  -- Vars (Maybe (Vars (a, Double))
-  . fmap (sequenceA . fmap sequenceA)
-  -- Vars (Maybe (a, Vars Double))
-  . runMaybeT
-  -- MaybeT Vars (a, Vars Double)
-  . runWriterT
-  -- WriterT (Vars Double) (MaybeT Vars) a
-  . mapWriterT (fmap (second (fmap runSF)))
+-- runPhysObj :: (a -> MaybePhysObj r a -> Vars (Maybe (a, Double))
+runPhysObj
+  :: PhysObj a a
+  -- -> (a -> (MaybeT (CorrectedT (Vars SF) Vars)) r)
+  -> (MaybeT (CorrectedT (Vars SF) Vars)) a
+runPhysObj p = runContT p pure
