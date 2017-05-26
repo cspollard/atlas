140d5e253ce22b9b7e3aa4db5858665014d1ba96
Chris Pollard <cspollard@gmail.com>
2017-05-26 12:01:35 +0100

    unhiding...

--- src/Atlas.hs
+++ src/Atlas.hs
@@ -8,5 +8,5 @@ import           Atlas.PhysObj          as X
 import           Atlas.Streaming        as X
 import           Atlas.Variation        as X
 import           Data.HEP.LorentzVector as X
-import           Data.Hist              as X hiding (Only(..), Pair(..))
-import           Data.YODA.Obj          as X hiding (Only (..), Pair (..))
+import           Data.Hist              as X
+import           Data.YODA.Obj          as X
